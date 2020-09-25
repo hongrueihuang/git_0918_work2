b794d1e7ca9d7bf041a86364bcfb7e25e5606b75
Ray <ray1682220@gmail.com>
2020-09-25 15:40:01 +0800

    新增功能二

--- all.js
+++ all.js
@@ -7,3 +7,11 @@ function multipleList() {
     }
 }
 multipleList()
+
+// BMI
+function getBMI(height,weight) {
+    let meterHeight = height / 100;
+    let bmi = (weight / (meterHeight * meterHeight)).toFixed(1);
+    console.log(bmi)
+}
+getBMI(184, 95)
