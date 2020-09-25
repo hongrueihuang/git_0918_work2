58cf2604792137445b6c8f6f6a0e32254afcf730
Ray <ray1682220@gmail.com>
2020-09-25 15:37:57 +0800

    新增功能一

---
+++ all.js
@@ -0,0 +1,9 @@
+// 乘法表
+function multipleList() {
+    for(let i = 3; i < 16; i++) {
+        for(let j = 1; j < 16; j++){
+            console.log(`${i} x ${j} = ${i * j}`)
+        }
+    }
+}
+multipleList()
